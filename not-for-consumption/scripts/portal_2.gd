10688bddba6c341b2d46c564f98a8e56dd18da9b
25657-burnside <25657@burnside.school.nz>
2026-06-22 10:50:14 +1200

    fixed dropper player movement

--- not-for-consumption/scripts/portal_2.gd
+++ not-for-consumption/scripts/portal_2.gd
@@ -1,4 +1,4 @@
 extends Area2D
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "player":
+	if body.name == "playerDROPPER":
 		get_tree().change_scene_to_file("res://scenes/Levels/Level 3.tscn")
