ace6a1d3e0e70a060f9ed40aa4cd25600850c4d8
25657-burnside <25657@burnside.school.nz>
2026-07-23 14:40:14 +1200

    fixed bile wave

---
+++ not-for-consumption/scripts/jump_pad.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+# Using @export lets you easily change the bounce power in the Inspector!
+@export var bounce_power = -600
+
+func _on_body_entered(body):
+	# Check if the thing stepping on the pad is the player
+	if body.is_in_group("player"):
+		# Immediately shoot the player upward
+		body.velocity.y = bounce_power
