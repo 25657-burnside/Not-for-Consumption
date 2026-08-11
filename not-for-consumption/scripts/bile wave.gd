20064d843c5ff6f5b4779f6cfd4a50484a445b3d
25657-burnside <25657@burnside.school.nz>
2026-08-12 10:11:57 +1200

    finishing level 3 TPODAYYYYUYY

--- not-for-consumption/scripts/bile wave.gd
+++ not-for-consumption/scripts/bile wave.gd
@@ -4,9 +4,14 @@ extends Area2D
 var speed = 200
 var direction = -1 # 1 moves right, -1 moves left
 var destroy_x_location = 386.0
+# How fast you want it to move (pixels per second)
+var risespeed = 70
 
+# The exact Y coordinate where you want it to stop
+var target_y = 470.0
 func _process(delta):
 	# Move horizontally across the screen
+	position.y = move_toward(position.y, target_y, speed * delta)
 	position.x += speed * direction * delta
 	if position.x < destroy_x_location:
 		queue_free()
